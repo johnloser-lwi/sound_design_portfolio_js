49829dc89ef0519b7fb8e8a126807a99e1b69bc5
johnloser <66107110+johnloser-lwi@users.noreply.github.com>
2025-07-27 21:51:23 -0700

    finish home screen

---
+++ js/header_footer.js
@@ -0,0 +1,28 @@
+const header = document.createElement("header");
+header.innerHTML = `
+    <div class="header-links">
+        <a class="header-links__title link" href="./index.html">Weiqin Yang Sound</a>
+        <nav class="header-links__nav">
+            <ul>
+                <li><a href="#" class="link">Projects</a></li>
+                <li><a href="#" class="link">More</a></li>
+            </ul>
+        </nav>
+    </div>
+`;
+header.classList.add("grid");
+document.querySelector("body").prepend(header);
+
+const footer = document.createElement("footer");
+footer.innerHTML = `
+    <div class="footer-socials">
+        <ul>
+            <li><a href="#"><img src="./assets/images/brands-github.svg" alt="Github"></a></li>
+            <li><a href="#"><img src="./assets/images/brands-instagram.svg" alt="Instagram"></a></li>
+            <li><a href="#"><img src="./assets/images/brands-linkedin.svg" alt="LinkedIn"></a></li>
+        </ul>
+    </div>
+    <p class="footer-copyright">&copy;2025 Weiqin Yang. All rights reserved.</p>
+`;
+footer.classList.add("grid");
+document.querySelector("body").append(footer);
---
+++ js/home_animation.js
@@ -0,0 +1,73 @@
+
+
+const setupParallaxScroll = () => {
+    // hero section
+    $(".hero").attr('data-parallaxus-transform',
+        `{
+            "0": {"opacity": 1, "translateY": "0%"},
+            "50": {"opacity": 1, "translateY": "0%"},
+            "80": {"opacity": 0.5, "translateY": "25%"},
+            "100": {"opacity": 0.5, "translateY": "15%"}
+        }`);
+    $(".hero-parallax__title-wrap").attr('data-parallaxus-transform',
+        `{
+            "0": {"opacity": 1, "scale": 1, "translateY": "0%"},
+            "30": {"opacity": 0.5, "scale": 0.95, "translateY": "5%"},
+            "100": {"opacity": 0, "scale": 0.95, "translateY": "5%"}
+        }`);
+
+    $(".hero-link__about-me__wraper").attr('data-parallaxus-transform',
+        `{
+            "0": {"opacity": 1},
+            "30": {"opacity": 0.5},
+            "100": {"opacity": 0}
+        }`);
+
+    // bio section
+    $(".bio-info__title, .bio-info__text").attr('data-parallaxus-transform',
+        `{
+            "0": {"translateX": "100%", "opacity":0},
+            "40": {"translateX": "0%", "opacity":1},
+            "100": {"translateX": "0%", "opacity":1}
+        }`);
+
+    $(".bio-info__button").attr('data-parallaxus-transform',
+        `{
+            "0": {"translateX": "600%", "opacity":0},
+            "40": {"translateX": "0%", "opacity":1},
+            "100": {"translateX": "0%", "opacity":1}
+        }`);
+    $(".bio img, .bio-title").attr('data-parallaxus-transform',
+        `{
+            "0": {"translateX": "-50%", "opacity":0},
+            "50": {"translateX": "0%", "opacity":1},
+            "80": {"translateX": "0%", "opacity":1},
+            "100": {"translateX": "0%", "opacity":1}
+        }`);
+
+    parallaxusSetup();
+}
+
+const setupBgVideo = () => {
+    $(".hero-parallax__video").bgVideo({
+        showPausePlay: false,
+        pauseAfter: 0,
+        fadeIn: 0,
+    });
+}
+
+const setupParallaxJs = () => {
+    $(".hero-parallax__layer1").attr("data-depth", "0.2");
+
+    let parallaxInstance = new Parallax(document.querySelector(".hero-parallax"), {
+        hoverOnly: true,
+    });
+
+    parallaxInstance.friction(0.5, 0.5);
+}
+
+$(() => {
+    setupBgVideo();
+    setupParallaxScroll();
+    setupParallaxJs();
+})
